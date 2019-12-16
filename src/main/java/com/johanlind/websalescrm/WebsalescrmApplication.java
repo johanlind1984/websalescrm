3c2533b612007a0988262d612995abc5fd6595fb
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2019-12-16 14:26:33 +0100

    Initial commit

---
+++ src/main/java/com/johanlind/websalescrm/WebsalescrmApplication.java
@@ -0,0 +1,13 @@
+package com.johanlind.websalescrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsalescrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsalescrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johanlind/websalescrm/WebsalescrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanlind.websalescrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsalescrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
