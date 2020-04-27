d67a3c0f0ec92eadb8476cdc0423e6a9c3f501ec
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-04-27 20:35:15 +0200

    kund har epost och buggfix

--- src/main/java/com/johanlind/websalescrm/Controller/MainViewController.java
+++ src/main/java/com/johanlind/websalescrm/Controller/MainViewController.java
@@ -5,8 +5,6 @@ import com.johanlind.websalescrm.Repository.RepositoryCustomer;
 import com.johanlind.websalescrm.Repository.RepositoryEmployee;
 import com.johanlind.websalescrm.Repository.RepositoryUser;
 import com.johanlind.websalescrm.Utility.WebSalesUtilities;
-import com.johanlind.websalescrm.entity.Comment;
-import com.johanlind.websalescrm.entity.Customer;
 import com.johanlind.websalescrm.entity.Employee;
 import com.johanlind.websalescrm.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,17 +33,6 @@ public class MainViewController {
 
     @RequestMapping("/employee/mainview")
     public String getCustomerListForMainView(Model theModel, Principal principal) {
-        Customer customer = repositoryCustomer.findById((long) 32).orElse(null);
-
-        for (Comment comment : customer.getCommentList()) {
-            System.out.println(comment.getCustomer().getName());
-            System.out.println(comment.getCommentId());
-            System.out.println(comment.getTimestamp());
-            System.out.println(comment.getComment());
-            System.out.println("========EOF==========");
-
-        }
-
         User user = repositoryUser.findByUserName(principal.getName());
         Employee employee = employeeRepository.findById(user.getId()).orElse(null);
         theModel.addAttribute("header", WebSalesUtilities.getHeaderString(user));
