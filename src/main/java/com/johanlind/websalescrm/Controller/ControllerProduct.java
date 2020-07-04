2530c878a89907a5f97b756a44a31e45a71474df
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-07-04 23:06:34 +0200

    fixade a href till rätt url och massa html kladd

--- src/main/java/com/johanlind/websalescrm/Controller/ControllerProduct.java
+++ src/main/java/com/johanlind/websalescrm/Controller/ControllerProduct.java
@@ -1,6 +1,8 @@
 package com.johanlind.websalescrm.Controller;
 
 import com.johanlind.websalescrm.Repository.RepositoryProduct;
+import com.johanlind.websalescrm.Repository.RepositoryUser;
+import com.johanlind.websalescrm.Utility.WebSalesUtilities;
 import com.johanlind.websalescrm.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -21,27 +24,34 @@ public class ControllerProduct {
     @Autowired
     private RepositoryProduct repositoryProduct;
 
+    @Autowired
+    private RepositoryUser repositoryUser;
+
     @RequestMapping("/crud/addproduct")
-    public String addProduct(Model theModel) {
+    public String addProduct(Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         theModel.addAttribute("productadded", new Product());
         return "product/crud/add-product";
     }
 
     @RequestMapping("/crud/productconfirmed")
-    public String confirmProduct(@ModelAttribute("productadded") Product product) {
+    public String confirmProduct(@ModelAttribute("productadded") Product product, Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         repositoryProduct.save(product);
         return "product/crud/confirmation-product-added";
     }
 
     @RequestMapping("/productlist")
-    public String listProducts(Model theModel) {
+    public String listProducts(Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         List<Product> productList = repositoryProduct.findAll();
         theModel.addAttribute("productlist", productList);
         return "product/list/product-list";
     }
 
     @RequestMapping(value="/crud/deleteproduct", method = RequestMethod.GET)
-    public String deleteProduct(@RequestParam("id") long productId, Model theModel) {
+    public String deleteProduct(@RequestParam("id") long productId, Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         repositoryProduct.deleteById(productId);
         List<Product> productList = repositoryProduct.findAll();
         theModel.addAttribute("productlist", productList);
@@ -49,7 +59,8 @@ public class ControllerProduct {
     }
 
     @RequestMapping(value="/crud/updateproductform", method = RequestMethod.GET)
-    public String updateProductForm(@RequestParam("id") long productId, Model theModel) {
+    public String updateProductForm(@RequestParam("id") long productId, Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         Product product = repositoryProduct.findById(productId).orElse(null);
         theModel.addAttribute("product", product);
         return "product/crud/update-product";
@@ -57,7 +68,8 @@ public class ControllerProduct {
 
 
     @RequestMapping(value="/updateproduct")
-    public String updateProduct(@ModelAttribute("product") Product product, Model theModel) {
+    public String updateProduct(@ModelAttribute("product") Product product, Model theModel, Principal principal) {
+        theModel.addAttribute("header", WebSalesUtilities.getHeaderString(repositoryUser.findByUserName(principal.getName())));
         repositoryProduct.save(product);
         List<Product> productList = repositoryProduct.findAll();
         theModel.addAttribute("productlist", productList);
